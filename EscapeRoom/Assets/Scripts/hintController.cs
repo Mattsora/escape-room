f4d87e27fe1ae46ad0279d6961031816a046c7ab
Evilman <timo02@me.com>
2019-09-26 10:53:18 +0200

    Added hint script

---
+++ EscapeRoom/Assets/Scripts/hintController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hintController : MonoBehaviour
+{
+    public float timer = 60.0f;
+    public Text hintOutput;
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 0.0f)
+        {
+            timerEnded();
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+       
+    }
+
+    private void timerEnded()
+    {
+        hintOutput.text = "Tip";
+        
+    }
+}
