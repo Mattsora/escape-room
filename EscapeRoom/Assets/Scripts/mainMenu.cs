09006eba0c839a628c98cf39bc69e07c59ae9116
Aaron <aaronvlak@hotmail.nl>
2019-09-26 12:21:37 +0200

    Main Menu, start + quit is working

---
+++ EscapeRoom/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+    public void LoadFirstLevel()
+    {
+        Debug.Log("START BUTTON IS PRESSED");
+        SceneManager.LoadScene("EscapeRoomAaronScene", LoadSceneMode.Single);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit button is pressed");
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
