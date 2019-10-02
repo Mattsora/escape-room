89ff0576800d83291f8af48331cb4cdf76f60fe1
Aaron <D267096@edu.rocwb.nl>
2019-10-02 07:58:09 -0700

    Fixed Several Bugs and added first level

--- EscapeRoom/Assets/TESTING/Zaid/ZaidScripts/modularGrabObject.cs
+++ EscapeRoom/Assets/TESTING/Zaid/ZaidScripts/modularGrabObject.cs
@@ -8,10 +8,14 @@ public class modularGrabObject : MonoBehaviour
     public bool beingHeld = false;
     public bool canHold = true;
     Rigidbody mRigidbody;
+    bool asleep;
     // Start is called before the first frame update
     void Start()
     {
         mRigidbody = GetComponent<Rigidbody>();
+        asleep = true;
+        mRigidbody.velocity = Vector3.zero;
+        mRigidbody.Sleep();
     }
 
     // Update is called once per frame
@@ -19,13 +23,20 @@ public class modularGrabObject : MonoBehaviour
     {
         if (holderObject != null)
         {
+            if(asleep)
+            {
+                mRigidbody.velocity = Vector3.zero;
+            }
             if (holderObject.GetComponent<modularGrabController>())
             {
                 if (beingHeld)
                 {
+                    asleep = false;
                     transform.position = holderObject.position;
                     mRigidbody.velocity = Vector3.zero;
-
+                    mRigidbody.transform.eulerAngles = holderObject.transform.eulerAngles;
+                    mRigidbody.useGravity = true;
+                    mRigidbody.detectCollisions = true;
                 }
                
             }
