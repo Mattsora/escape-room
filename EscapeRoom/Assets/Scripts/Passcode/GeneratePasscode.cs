033a5f58fab11e2e3cd3e3e94b9b803f9536cdb7
Aaron <D267096@edu.rocwb.nl>
2019-10-21 02:42:18 -0700

    test

---
+++ EscapeRoom/Assets/Scripts/Passcode/GeneratePasscode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePasscode : MonoBehaviour
+{
+    public int[] Numbers = new int[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Randomize();
+    }
+
+    // Will randomize unless already set
+    void Randomize()
+    {     
+        for (int i = 0; i < 4; i++)
+        {
+            Numbers[i] = Random.Range(0, 9);
+        }
+    }
+
+}
