cd580e46e364e9cf23d27f142a6ccce62f67a81a
Evilman <timo02@me.com>
2019-10-21 08:59:10 +0200

    game password & stuff

---
+++ EscapeRoom/Assets/Scripts/Passcode/SetPasscode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPasscode : MonoBehaviour
+{
+    public GeneratePasscode generatedCode;
+    public TextMesh Text;
+    public int Current;
+    int Number;
+    string Format;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Number = generatedCode.Numbers[Current];
+        if (Current == 0)
+        {
+            Format = string.Format("{0} - - -", Number);
+        }
+        else if (Current == 1)
+        {
+            Format = string.Format("- {0} - -", Number);
+        }
+        else if (Current == 2)
+        {
+            Format = string.Format("- - {0} -", Number);
+        }
+        else if (Current == 3)
+        {
+            Format = string.Format("- - - {0}", Number);
+        }
+        else
+        {
+            Format = null;
+        }
+        Text.text = Format;
+    }
+}
